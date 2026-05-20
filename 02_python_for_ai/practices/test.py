d1627a2f7587d84763644d3c2f1a33fa0fbb297e
lemin0x <medleminy23@gmail.com>
2026-05-20 22:37:05 +0100

    test script

---
+++ 02_python_for_ai/practices/test.py
@@ -0,0 +1,10 @@
+def date_to_season(date):
+    month = date.month
+    if (month == 12) or (month == 1) or (month == 2):
+        return 'Summer'
+    elif (month == 3) or (month == 4) or (month == 5):
+        return 'Autumn'
+    elif (month == 6) or (month == 7) or (month == 8):
+        return 'Winter'
+    elif (month == 9) or (month == 10) or (month == 11):
+        return 'Spring'
